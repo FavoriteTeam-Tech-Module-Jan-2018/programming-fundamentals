600ce931cd207d632ceb7158677445011793c7b9
Ivan Peevski <i.peevski@abv.bg>
2018-03-10 18:15:42 +0200

    Added Problem 2

---
+++ Intro-and-basic-syntax/exercise/Problem 2/RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double rect_area = a * b;
+            Console.WriteLine($"{rect_area:F2}");
+        }
+    }
+}
