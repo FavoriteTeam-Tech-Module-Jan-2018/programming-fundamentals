3c96b0abc5a7220e68ccdc2ae64870f9da4d5256
Practically Nothing <practicallynothingatall@gmail.com>
2018-03-11 13:49:24 +0200

    Added Problem 4

---
+++ Intro-and-basic-syntax/exercise/Problem 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string name = Console.ReadLine();
+			int volume = int.Parse(Console.ReadLine());
+			int energy = int.Parse(Console.ReadLine());
+			int sugar = int.Parse(Console.ReadLine());
+
+			int kCalPer100ml  = (int) (volume/100.0 * energy);
+
+			int sugarPer100ml = (int) (volume/100.0 * sugar);
+
+			Console.WriteLine(
+					"{0}ml {1}:\n{2}kcal, {3}g sugars",
+					volume, name, kCalPer100ml, sugarPer100ml);
+        }
+    }
+}
