b7997f4ce0c39b3258011a29176f5d605c3f867e
Toma Tomov <toooom@mail.bg>
2018-03-11 14:58:48 +0200

    Document the code of the Problem 2.

--- Intro-and-basic-syntax/exercise/Problem 2/RectangleArea.cs
+++ Intro-and-basic-syntax/exercise/Problem 2/RectangleArea.cs
@@ -6,14 +6,14 @@ namespace _02.RectangleArea
     {
         static void Main(string[] args)
         {
-           // We take the variables from the console.
+           // We take the variables from the console
             double a = double.Parse(Console.ReadLine());
             double b = double.Parse(Console.ReadLine());
 
-            // Calculate the rectangle area.
+            // Calculate the rectangle area
             double rect_area = a * b;
 
-          // Print the reslt.
+          // Print the reslt
             Console.WriteLine($"{rect_area:F2}");
         }
     }
