34cd5a3a9ee8be6bd602366c3379f3a17db44ad1
chizmyak <chizmyak@gmail.com>
2018-03-12 14:45:21 +0200

    Documented Problem 4/Program.cs

--- Intro-and-basic-syntax/exercise/Problem 4/Program.cs
+++ Intro-and-basic-syntax/exercise/Problem 4/Program.cs
@@ -6,15 +6,20 @@ namespace Problem_4
     {
         static void Main(string[] args)
         {
+            //We recieve a product name from the Console
 			string name = Console.ReadLine();
+
+            //On the next three lines we recieve the product contents
 			int volume = int.Parse(Console.ReadLine());
 			int energy = int.Parse(Console.ReadLine());
 			int sugar = int.Parse(Console.ReadLine());
 
+            //Here we calculate the sugar and energy content for the given volume
 			int kCalPer100ml  = (int) (volume/100.0 * energy);
 
 			int sugarPer100ml = (int) (volume/100.0 * sugar);
 
+            //TO DO
 			Console.WriteLine(
 					"{0}ml {1}:\n{2}kcal, {3}g sugars",
 					volume, name, kCalPer100ml, sugarPer100ml);
