fb5e78818209088c57bb320baa995a5d84710b56
Ivan Peevski <i.peevski@abv.bg>
2018-03-12 17:56:21 +0200

    Documented Problem3

--- Intro-and-basic-syntax/exercise/Problem3/MilesToKm/Program.cs
+++ Intro-and-basic-syntax/exercise/Problem3/MilesToKm/Program.cs
@@ -6,9 +6,13 @@ namespace Problem3
     {
         static void Main(string[] args)
         {
+            //Read the variable from the Console
             double miles = double.Parse(Console.ReadLine());
+
+            //Calculate the kilometres
             double kilometers = miles * 1.60934;
 
+            //Print the result
             Console.WriteLine($"{kilometers:F2}");
         }
     }
