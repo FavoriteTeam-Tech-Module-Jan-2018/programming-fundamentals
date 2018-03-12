bfacae45f3aa43c23a0a75a99149769043a575b0
Toma Tomov <toooom@mail.bg>
2018-03-12 21:46:36 +0200

    Change a variable names in Problem 4

--- Intro-and-basic-syntax/exercise/Problem 4/Program.cs
+++ Intro-and-basic-syntax/exercise/Problem 4/Program.cs
@@ -7,22 +7,22 @@ namespace Problem_4
         static void Main(string[] args)
         {
             //We recieve a product name from the Console
-			string name = Console.ReadLine();
+            string name1 = Console.ReadLine();
 
             //On the next three lines we recieve the product contents
-			int volume = int.Parse(Console.ReadLine());
-			int energy = int.Parse(Console.ReadLine());
-			int sugar = int.Parse(Console.ReadLine());
+            int volume1 = int.Parse(Console.ReadLine());
+            int energy1 = int.Parse(Console.ReadLine());
+            int sugar1 = int.Parse(Console.ReadLine());
 
             //Here we calculate the sugar and energy content for the given volume
-			int kCalPer100ml  = (int) (volume/100.0 * energy);
+            int kCalloriesPer100ml = (int)(volume1 / 100.0 * energy1);
 
-			int sugarPer100ml = (int) (volume/100.0 * sugar);
+            int sugar100ml = (int)(volume1 / 100.0 * sugar1);
 
             //TO DO
-			Console.WriteLine(
-					"{0}ml {1}:\n{2}kcal, {3}g sugars",
-					volume, name, kCalPer100ml, sugarPer100ml);
+            Console.WriteLine(
+                    "{0}ml {1}:\n{2}kcal, {3}g sugars",
+                    volume1, name1, kCalloriesPer100ml, sugar100ml);
         }
     }
 }
