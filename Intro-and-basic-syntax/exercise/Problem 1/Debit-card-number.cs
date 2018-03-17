0cadfe6f6185db2a0413c3f394eed1b18521d029
Practically Nothing <practicallynothingatall@gmail.com>
2018-03-17 23:13:41 +0200

    Document Problem 1

--- Intro-and-basic-syntax/exercise/Problem 1/Debit-card-number.cs
+++ Intro-and-basic-syntax/exercise/Problem 1/Debit-card-number.cs
@@ -6,11 +6,13 @@ namespace Debit_card_Number
     {
         static void Main(string[] args)
         {
+			// We read the four numbers of the debit card
             int firstNum = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
             int thirdNumber = int.Parse(Console.ReadLine());
             int fourthNumber = int.Parse(Console.ReadLine());
 
+			// And then print them with at least four digits
             Console.WriteLine($"{firstNum:D4} {secondNumber:D4} {thirdNumber:D4} {fourthNumber:D4}");
         }
     }
