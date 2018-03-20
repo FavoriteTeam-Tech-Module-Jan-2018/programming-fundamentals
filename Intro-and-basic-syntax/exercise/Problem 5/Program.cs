cb5e0ac5940287a8f969474be42726e6a21f70a3
Ivan Peevski <i.peevski@abv.bg>
2018-03-20 19:46:25 +0200

    Changed a variable name

--- Intro-and-basic-syntax/exercise/Problem 5/Program.cs
+++ Intro-and-basic-syntax/exercise/Problem 5/Program.cs
@@ -5,7 +5,7 @@ class Program {
 
 		// We read the character's name, as well as his statistics
 		string name = Console.ReadLine();
-		int currHealth = int.Parse(Console.ReadLine()),
+		int currentHealth = int.Parse(Console.ReadLine()),
 			currEnergy = int.Parse(Console.ReadLine()),
 			maxHealth  = int.Parse(Console.ReadLine()),
 			maxEnergy  = int.Parse(Console.ReadLine());
@@ -13,8 +13,8 @@ class Program {
 		// And we print them in the required format
 		Console.WriteLine("Name: {0}\nHealth: |{1}{2}|\nEnergy: |{3}{4}|",
 				name,
-				new string('|', currHealth),
-				new string('.', maxHealth - currHealth),
+				new string('|', currentHealth),
+				new string('.', maxHealth - currentHealth),
 				new string('|', currEnergy),
 				new string('.', maxEnergy - currEnergy));
 	}
